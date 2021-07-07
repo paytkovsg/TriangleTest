ca1eca0e0d7a66c7881a2f14b67a16d174cf037d
paytkovsg <paytkov.s@gmail.com>
2021-07-07 19:24:12 +0500

    ДЗ 4

---
+++ src/main/java/org/example/main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class main {
+
+
+
+    public static void main(String[] args) {
+        Triangle t = new Triangle();
+        System.out.println(t.squareTriangle(3, 4, 5));
+    }
+}
